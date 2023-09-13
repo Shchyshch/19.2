030d7eca81ca39619a167e43ccafeadab273fef2
Ildar Gabdullin <ildar8803@gmail.com>
2023-09-13 03:04:33 +0300

    add 20.2 homework

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all(),
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+    }
+    return render(request, 'catalog/product.html', context)
